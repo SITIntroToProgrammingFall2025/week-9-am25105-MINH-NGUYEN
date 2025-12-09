afa58d54c2a9a88c212380204c54fc2ab83ef937
am25105-MINH-NGUYEN <am25105@sic.shibaura-it.ac.jp>
2025-12-09 18:17:27 +0900

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -3,17 +3,18 @@ int main() {
     int a[2][2];
     int i, j;
     for (i = 0; i < 2;i++) {
-        for (j = 0; i < 2; j++) {
+        for (j = 0; j < 2; j++) {
             scanf("%d", &a[i][j]);
         }
     }
     printf("You entered\n");
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++) {
-            printf("%d", a[i][j]);
+            printf("%d ", a[i][j]);
         }
         printf("\n");
     }
     printf("Determinant is %d", (a[0][0] * a[1][1]) - (a[1][0] * a[0][1]));
 }
+
 
